9088978e1912aab7314e9dcd1bbd5dd57b9102fa
Daniel Li <neolee11@yahoo.com>
2015-06-24 18:34:29 -0400

    Initial commit

---
+++ data.js
@@ -0,0 +1,17 @@
+var books = [
+    {
+        id: 1,
+        name: 'book1',
+        author: 'someone1'
+    },
+    {
+        id: 2,
+        name: 'book2',
+        author: 'john'
+    },
+    {
+        id: 3,
+        name: 'book3',
+        author: 'brain'
+    }
+];
